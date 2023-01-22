6992c309f6973fcd6d05c0cb57ce6d9965af5f51
xenon <768527756@qq.com>
2023-01-22 16:20:31 +0800

    crud完善

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -2,9 +2,12 @@ package com.atguigu.gulimall.product;
 
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
@@ -14,8 +17,10 @@ class GulimallProductApplicationTests {
     @Test
     void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
-        brandEntity.setName("华为");
-        brandService.save(brandEntity);
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        list.forEach((item)->{
+            System.out.println(item);
+        });
     }
 
 }
